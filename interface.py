08ccaadea653e6bc355fdadc0cf766e36f3e0312
Gagandeep Singh <57759605+50R1Paps@users.noreply.github.com>
2024-02-23 16:20:56 +0100

    first timer version

---
+++ interface.py
@@ -0,0 +1,36 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.clock import Clock
+import datetime
+
+class MyApp(App):
+    def build(self):
+        time = 30
+        self.target_time = datetime.datetime.now() + datetime.timedelta(seconds=time+3)
+        self.countdown_active = False
+        self.main_box = BoxLayout(orientation='vertical')
+        
+        self.button = Button(text='Start Countdown', font_size=100)
+        self.main_box.add_widget(self.button)
+    
+        self.button.bind(on_press=self.start_countdown)
+        
+        return self.main_box
+    
+    def start_countdown(self, button):
+        self.countdown_active = True
+        Clock.schedule_interval(self.timer, 1.0)
+
+    def timer(self, dt):
+        if self.countdown_active:
+            current_time = datetime.datetime.now()
+            remaining_time = self.target_time - current_time
+            if remaining_time.total_seconds() <= 0:
+                self.button.text = 'Countdown finished!'
+                self.countdown_active = False
+            else:
+                self.button.text = str(int(remaining_time.total_seconds()))  # Mostra solo la parte intera
+
+if __name__ == '__main__':
+    MyApp().run()
